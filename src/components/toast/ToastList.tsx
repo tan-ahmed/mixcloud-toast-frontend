5567b187715a46d820865107ea29557286ac817d
tan-ahmed <tannerahmed@gmail.com>
2023-12-07 20:18:55 +0000

    tidied up

--- src/components/toast/ToastList.tsx
+++ src/components/toast/ToastList.tsx
@@ -1,19 +1,15 @@
 // ToastList.tsx
-import { useState, useCallback, useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import Toast from "./Toast";
 import blankDp from "../../assets/blank-dp.png";
-import useGetUserFollowing from "../../hooks/useGetFollowing";
+import { ToastProps } from "../../views/Home";
 
-export interface ToastProps {
-    id: number;
-    title: string;
-    picture?: string;
+interface ToastListProps {
+    allToasts: ToastProps[];
+    setAllToasts: React.Dispatch<React.SetStateAction<ToastProps[]>>;
 }
 
-const ToastList = () => {
-    const [allToasts, setAllToasts] = useState<ToastProps[]>([]);
-    const { data, isLoading, error } = useGetUserFollowing("spartacus");
-
+const ToastList = ({ allToasts, setAllToasts }: ToastListProps) => {
     const deleteToast = useCallback(
         (id: number) => {
             setAllToasts((prevList) => {
@@ -36,30 +32,8 @@ const ToastList = () => {
         };
     }, [allToasts, deleteToast]);
 
-    const addToast = ({ title, picture }: Omit<ToastProps, "id">) => {
-        const newId = Date.now(); // Use a unique identifier, for example, timestamp
-        const newToast = {
-            id: newId,
-            title,
-            picture,
-        };
-
-        setAllToasts((prevList) => [...prevList, newToast]);
-    };
-
     return (
         <div>
-            {data &&
-                data.data.length > 0 &&
-                data?.data.map((user) => (
-                    <button type="button" key={user.key} onClick={() => addToast({ title: user.username, picture: user.pictures.medium_mobile })}>
-                        <div className="flex items-center space-x-2">
-                            <img className="w-10 h-10 rounded-full" src={user.pictures.medium_mobile} alt={user.username} loading="lazy" />
-                            <div className="text-gray-800 text-base leading-snug">{user.username}</div>
-                        </div>
-                    </button>
-                ))}
-
             {allToasts.map((toast) => (
                 <Toast key={toast.id} id={toast.id} title={toast.title} picture={toast.picture || blankDp} onClose={() => deleteToast(toast.id)} />
             ))}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,13 +7,12 @@ import ToastList from "../components/toast/ToastList";
 export interface ToastProps {
     id: number;
     title: string;
-    description: string;
-    picture: string;
-    onClick: () => void;
+    picture?: string;
 }
 
 const Home = () => {
     const { data, isLoading, error } = useGetUserFollowing("spartacus");
+    const [allToasts, setAllToasts] = useState<ToastProps[]>([]);
 
     if (isLoading) {
         return <p>Loading...</p>;
@@ -23,19 +22,27 @@ const Home = () => {
         return <p>{error.message}</p>;
     }
 
-    const description = "Has gone live - watch now";
+    const addToast = ({ title, picture }: Omit<ToastProps, "id">) => {
+        const newId = Date.now(); // Use a unique identifier, for example, timestamp
+        const newToast = {
+            id: newId,
+            title,
+            picture,
+        };
+
+        setAllToasts((prevList) => [...prevList, newToast]);
+    };
 
     return (
         <Layout>
             <div className="container space-y-5 my-6">
                 <h1 className="font-bold text-xl">Following</h1>
-                <ToastList />
+                <ToastList allToasts={allToasts} setAllToasts={setAllToasts} />
 
-                {/* <pre>{JSON.stringify(toasts, null, 2)}</pre> */}
                 {data &&
                     data.data.length > 0 &&
                     data?.data.map((user) => (
-                        <button type="button" key={user.key} onClick={() => addToast(user.username, description, user.pictures.medium_mobile)}>
+                        <button type="button" key={user.key} onClick={() => addToast({ title: user.username, picture: user.pictures.medium_mobile })}>
                             <div className="flex items-center space-x-2">
                                 <img className="w-10 h-10 rounded-full" src={user.pictures.medium_mobile} alt={user.username} loading="lazy" />
                                 <div className="text-gray-800 text-base leading-snug">{user.username}</div>
