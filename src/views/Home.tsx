f793a1358ebd114eba39138752f96e2e4ad23f8f
tan-ahmed <tannerahmed@gmail.com>
2023-12-08 19:02:02 +0000

    chore: tidy CSS

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -22,17 +22,11 @@ const Home = () => {
             <div className="container space-y-5 my-6">
                 <h1 className="font-bold text-xl">{username}</h1>
 
-                <button type="button" onClick={() => addToast({ title: "tan" })} className="p-1 rounded shadow">
-                    <div className="flex items-center space-x-2">
-                        <div className="text-gray-800 text-base leading-snug">Open toast for tan</div>
-                    </div>
-                </button>
-
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-2 xl:grid-cols-3">
                     {data &&
                         data.data.length > 0 &&
                         data?.data.map((user) => (
-                            <button type="button" key={user.key} onClick={() => addToast({ title: user.username, picture: user.pictures.medium_mobile })} className="p-1 rounded shadow">
+                            <button type="button" key={user.key} onClick={() => addToast({ title: user.username, picture: user.pictures.medium_mobile })} className="p-1 rounded border">
                                 <div className="flex items-center space-x-2">
                                     <img className="w-10 h-10 rounded-full" src={user.pictures.medium_mobile} alt={user.username} loading="lazy" />
                                     <div className="text-gray-800 text-base leading-snug">Open toast for {user.username}</div>
