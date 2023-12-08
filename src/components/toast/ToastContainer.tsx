7d2f47280d96736e5ed35be13dbf1fd192431da8
tan-ahmed <tannerahmed@gmail.com>
2023-12-08 16:34:12 +0000

    update container

--- src/components/toast/ToastContainer.tsx
+++ src/components/toast/ToastContainer.tsx
@@ -4,15 +4,10 @@ import blankDp from "../../assets/blank-dp.png";
 import { useToast } from "../../hooks/useToast";
 
 const ToastContainer = () => {
-    const { allToasts, addToast, deleteToast } = useToast();
+    const { allToasts, deleteToast } = useToast();
 
     return (
         <div className="fixed top-4 right-3 z-50 transition-all">
-            <button type="button" onClick={() => addToast({ title: "tan" })} className="p-1 rounded shadow">
-                <div className="flex items-center space-x-2">
-                    <div className="text-gray-800 text-base leading-snug">Open toast for test</div>
-                </div>
-            </button>
             {allToasts.slice(0, 3).map((toast) => (
                 <Toast key={toast.id} id={toast.id} title={toast.title} picture={toast.picture || blankDp} onClose={() => deleteToast(toast.id)} />
             ))}
