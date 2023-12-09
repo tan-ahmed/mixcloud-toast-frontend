e29e7189b2cfe6598b78d8c4d9bd42729c1b246b
tan-ahmed <tannerahmed@gmail.com>
2023-12-09 15:47:38 +0000

    update anime

--- src/components/toast/ToastContainer.tsx
+++ src/components/toast/ToastContainer.tsx
@@ -7,11 +7,17 @@ import { useToast } from "../../hooks/useToast";
 const ToastContainer = () => {
     const { allToasts, deleteToast } = useToast();
 
+    const toastVariants = {
+        hidden: { opacity: 0, transform: "translateY(16px)" },
+        visible: { opacity: 1, transform: "translateY(0)" },
+        exit: { opacity: 0, transform: "translateY(-16px)", transition: { duration: 0.25 } },
+    };
+
     return (
         <AnimatePresence>
             <div className="fixed top-24 right-3 z-50">
                 {allToasts.slice(0, 3).map((toast) => (
-                    <motion.div key={toast.id} initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1 } }} exit={{ opacity: 0, transition: { duration: 1 } }}>
+                    <motion.div key={toast.id} variants={toastVariants} initial="hidden" animate="visible" exit="exit">
                         <Toast id={toast.id} title={toast.title} picture={toast.picture || blankDp} onClose={() => deleteToast(toast.id)} />
                     </motion.div>
                 ))}
